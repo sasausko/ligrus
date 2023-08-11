b3984fcd20159deea62fad94fd7dc3a21625099a
“sasausko” <sadosadoushko@gmail.com>
2023-08-11 19:05:38 +0300

    add pico and fix styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :set_theme
+
+  private
+  
+  def set_theme
+    if params[:theme].present?
+      theme = params[:theme].to_sym
+      # session[:theme] = theme
+      cookies[:theme] = theme
+      redirect_to(request.referrer || root_path)
+    end
+  end
 end
